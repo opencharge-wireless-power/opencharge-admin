1e45f8e968ed2f762f3a63a1e091229958f8898d
Phebeonv <phebeon.vera@gmail.com>
2025-12-12 11:55:58 +0200

    Update file paths in components.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import path from 'path'
-import { fileURLToPath } from 'url'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
