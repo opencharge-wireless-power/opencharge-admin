1c7d24c73d0978b520c045385463641d088d3895
Phebeonv <phebeon.vera@gmail.com>
2025-12-12 11:27:55 +0200

    Update import path: Change index.css import to styles/index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
-import "./index.css"
+import "./styles/index.css"
 import "leaflet/dist/leaflet.css";
 
 /*const theme = createTheme({
